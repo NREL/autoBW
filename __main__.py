fe7f306566ca61e0cc3c4f8a37563a4e14198bd8
rjhanes <rebecca.hanes@nrel.gov>
2021-11-08 16:22:30 -0700

    adds logging and initial activity editing

--- __main__.py
+++ __main__.py
@@ -1,22 +1,31 @@
-import brightway2 as bw
-import yaml
 import argparse
 import os
-import pickle
+import yaml
 import logging
 
-from project import Project
+import brightway2 as bw
+import pandas as pd
 
-# @TODO Check that the script path in the configuration is correct
+
 
 # set up arguments for command line running
 parser = argparse.ArgumentParser(description='Execute automatic Brightway LCIA')
 parser.add_argument('--data', help='Path to data directory.')
 parser.add_argument('--config', help='Name of local config file.')
-args = parser.parse_args()
+
+# Set up logger
+# @TODO Generate unique log file names for each run?
+logging.basicConfig(
+    filename=os.path.join(
+        parser.parse_args().data,
+        'autobw.log'
+    ),
+    level=logging.INFO
+)
 
 # read in config (YAML) file with error handling; get variable groups
-config_yaml_filename = os.path.join(args.data, args.config)
+config_yaml_filename = os.path.join(parser.parse_args().data,
+                                    parser.parse_args().config)
 try:
     with open(config_yaml_filename, 'r') as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
@@ -24,20 +33,21 @@ try:
         fileIO = config.get('fileIO', {})
         project = config.get('project', {})
 except IOError as err:
-    print(f'Could not open {config_yaml_filename} for configuration.')
+    logging.error(msg=f'Could not open {config_yaml_filename} for configuration.')
     exit(1)
 
 # Project setup
-_name = project.get('name')
+_project = project.get('name')
 
 # if a new project is being created, instantiate it
-if flags.get('create_new_project') and _name in list(bw.projects):
-    # If the project already exists, throw an error.
-    print(f'Project {_name} already exists.')
+
+# If the project already exists, throw an error.
+if flags.get('create_new_project') and _project in list(bw.projects):
+    logging.error(msg=f'Project {_project} already exists.')
     exit(1)
 
 # Project setup
-bw.projects.set_current(project.get('name'))
+bw.projects.set_current(_project)
 
 # @TODO Log current project name and directory
 
@@ -46,9 +56,9 @@ bw.bw2setup()
 
 # Imported database setup
 # Get the dictionary of database names:locations
-_databases = project.get('database_sources')
-
-# @TODO Log list of databases in the current project
+_databases = project.get('databases')
+_db_list = [key for key,value in bw.databases.items()]
+logging.info(msg=f'{_project} databases are {_db_list}')
 
 # trim the database dictionary so it contains only those databases
 # that don't already exist
@@ -63,38 +73,107 @@ _db_create = {
 if len(_db_create) > 0:
     # If _db_create contains elements, then these are datbases that
     # don't exist and must be imported and postprocessed.
-    for _name, _loc in _db_create.items():
+    for _db, _loc in _db_create.items():
+        logging.info(msg=f'Importing {_db} from {_loc}')
         # @TODO Add database format parameter for selecting the import method
         # Excel importer, FORWAST, others?
-        _imported_db = bw.SingleOutputEcospold2Importer(
-            _loc,
-            _name
-        )
-        _imported_db.apply_strategies()
-        _imported_db.statistics()
-        _imported_db.write_database()
+        try:
+            _imported_db = bw.SingleOutputEcospold2Importer(
+                os.path.abspath(_loc),
+                _db
+            )
+            if _db in bw.databases:
+                logging.info(msg=f'Successfully imported {_db}')
+                logging.info(msg=f'Postprocessing {_db}')
+                _imported_db.apply_strategies()
+                _imported_db.statistics()
+                _imported_db.write_database()
+            else:
+                logging.warning(msg=f'Failed to import {_db}')
+        except AssertionError:
+            # @todo log the exact assertion error
+            logging.error(msg=f'Brightway AssertionError')
 
-    # @TODO Log "databases updated", list of databases in the current project
+
+    _db_list = [key for key,value in bw.databases]
+
+    logging.info(msg=f'{_project} databases updated to {_db_list}')
+else:
+    logging.info(msg='No new databases to add')
 
 
 # Activity and exchange editing
 
-# @TODO: Log "before" status of database
+# @TODO: Log "before" status of database?
 
-# Create new activities
-    # Generate unique code and store it somewhere
+# Import edit information from template
+try:
+    _create_activities = pd.read_excel(
+        io=os.path.join(fileIO.get('data_directory'),fileIO.get('db_edits')),
+        sheet_name='Create Activities'
+    )
+except ValueError:
+    logging.warning(msg='Create Activities sheet not found')
+    _create_activities = pd.DataFrame()
 
-    # If the exact activity already exists, throw a warning and move on
+try:
+    _delete_exchanges = pd.read_excel(
+        io=os.path.join(fileIO.get('data_directory'),fileIO.get('db_edits')),
+        sheet_name='Delete Exchanges'
+    )
+except ValueError:
+    logging.warning(msg='Delete Exchanges sheet not found')
+    _delete_exchanges = pd.DataFrame()
 
-# Delete exchanges from existing activities
-    # If the activity does not exist
-    # If the exchange in the activity does not exist
+try:
+    _add_exchanges = pd.read_excel(
+        io=os.path.join(fileIO.get('data_directory'),fileIO.get('db_edits')),
+        sheet_name='Add Exchanges'
+    )
+except ValueError:
+    logging.warning(msg='Add Exchanges sheet not found')
+    _add_exchanges = pd.DataFrame()
 
-# Add exchanges to existing activities
-    # If the activity does not exist
-    # If the exchange already exists
+
+# Create new activities if needed
+
+if not _create_activities.empty:
+    # Error handling: If the activity already exists, throw a warning and move on
+    # @TODO List comprehension method for searching all databases?
+    _checkdb = bw.Database([key for key,value in bw.databases.items()][0])
+    _duplicate_act = [
+        act
+        for act in _checkdb
+        if act["name"] in _create_activities.Name.tolist()
+           and act['type'] != 'emission'
+    ]
+    
+    if len(_duplicate_act) > 0:
+        logging.warning(msg=f'Duplicate activities found: {_duplicate_act}')
+        # @TODO Remove the duplicates from the _create_activities data frame
+
+
+    # After removing duplicate activities,
+    # Step 1: Generate unique code and store it in _create_activities.Code
+    _create_activities.Code = _create_activities.Name.replace(' ', '')
+
+    #logging.info(msg=f'Creating activities: {_create_activities}')
+
+
+if not _delete_exchanges.empty:
+    pass
+    # Delete exchanges from existing activities
+        # If the activity does not exist
+        # If the exchange in the activity does not exist
+
+if not _add_exchanges.empty:
+    pass
+    # Add exchanges to existing activities
+        # If the activity does not exist
+        # If the exchange already exists
 
 # @TODO: Log "after" status and record changes made
+
 
 # Methods setup
 
